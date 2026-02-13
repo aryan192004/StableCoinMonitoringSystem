c7907dd72008b2aa1a27b88afe4786abd70441e2
vasudhaagrawal2024-byte <vasudha.agrawal2024@vitstudent.ac.in>
2026-02-13 10:57:37 +0530

    added the dark theme

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-background text-textPrimary min-h-screen`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
